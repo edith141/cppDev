41310076e5733a99a6cdf482b94ecec0b7318703
edith141 <ifarooq20@gmail.com>
2022-07-04 13:22:02 +0530

    hw

--- hw.cpp
+++ hw.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
+    int a = 10;
+    float fa = 12.2;
+    double da = 12.23;
+    char ca = 'A';
+    bool ab = 1;
     std::cout << "HW!" << std::endl;
+    std::cout << a << fa << da << ca << ab << std::endl;
+
     std::cin.get();
 }
