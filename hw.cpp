9800841028fa87c1d01d9fc1ba104c1bbf823cf9
edith141 <ifarooq20@gmail.com>
2022-07-04 17:26:13 +0530

    changes

--- hw.cpp
+++ hw.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+// #include <hm.cpp>
+
+
+double mulIt(double num) {
+    return (num * 2);
+}
+
+int sumM(int, int);
 
 int main() {
-    int a = 10;
-    float fa = 12.2;
-    double da = 12.23;
-    char ca = 'A';
-    bool ab = 1;
+    int a {10} ;
+    float fa {12.2};
+    double da {12.23};
+    char ca {'A'};
+    bool ab {1};
+    int unint {};
     std::cout << "HW!" << std::endl;
-    std::cout << a << fa << da << ca << ab << std::endl;
-
+    std::cout << (a * a) << '\n' << fa << '\n' << da  << '\n' << ca  << '\n' << ab << std::endl;
+    std::cout << "UNINT:" << unint << '\n' ;
+    std::cout << "\n" << mulIt(a) << "\n" << sumM(1,4);
+    7;
+    'a';
     std::cin.get();
 }
