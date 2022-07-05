301cdc8f86ce8e91985e3831674c1347ae70ee9a
edith141 <ifarooq20@gmail.com>
2022-07-05 13:12:10 +0530

    refs and ptrs

--- hm.cpp
+++ hm.cpp
@@ -1,5 +1,15 @@
 int sumM(int x, int y) 
 {
+    // *sum = x + y;
     return x + y;
 }
 
+void incMWithPtr(int* x) 
+{
+    *x+=1;
+}
+
+void incMWithRef(int& x) 
+{
+    x+=1;
+}
--- hw.cpp
+++ hw.cpp
@@ -7,10 +7,13 @@ double mulIt(double num) {
 }
 
 int sumM(int, int);
+void incMWithPtr(int* x);
+void incMWithRef(int& x);
 
 int main() {
     int a {10} ;
     float fa {12.2};
+    int z {00};
     double da {12.23};
     char ca {'A'};
     bool ab {1};
@@ -18,8 +21,17 @@ int main() {
     std::cout << "HW!" << std::endl;
     std::cout << (a * a) << '\n' << fa << '\n' << da  << '\n' << ca  << '\n' << ab << std::endl;
     std::cout << "UNINT:" << unint << '\n' ;
-    std::cout << "\n" << mulIt(a) << "\n" << sumM(1,4);
+    std::cout <<"\n Z VAR: " << z;
+    std::cout << "\n" << mulIt(a) << "\n" << sumM(1,8);
+    incMWithPtr(&z);
+    std::cout <<"\n Z VAR inc_ptr: " << z;
+    z = 12;
+    std::cout <<"\n Z VAR: " << z;
+    incMWithRef(z);
+    std::cout <<"\n Z VAR inc_ref:" << z;
+    // std::cout <<"\n Z VAR: " << z;
     7;
     'a';
     std::cin.get();
 }
+
